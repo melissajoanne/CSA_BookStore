7b19d536f85f02b5dd36357657fe831a83add10c
melzjoanne <melzjoanne@gmail.com>
2025-03-06 14:38:43 +0530

    File structure added

---
+++ src/main/java/lk/ac/iit/Models/Customer.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lk.ac.iit.bookstore.Models;
+
+/**
+ *
+ * @author melzjoanne
+ */
+public class Customer {
+    
+}
