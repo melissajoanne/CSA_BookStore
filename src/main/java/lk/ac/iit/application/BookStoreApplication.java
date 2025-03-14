65a2b77206506fe6db9128fc56ff283be0ccae1b
melzjoanne <melzjoanne@gmail.com>
2025-03-14 11:37:16 +0530

    Setup JAX-RS Application

---
+++ src/main/java/lk/ac/iit/application/BookStoreApplication.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lk.ac.iit.application;
+
+/**
+ *
+ * @author melzjoanne
+ */
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class BookStoreApplication extends Application {
+    
+}
