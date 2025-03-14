90d34b27811156b7d279599b7759f7c6bf005c30
melzjoanne <melzjoanne@gmail.com>
2025-03-14 11:21:07 +0530

    Folder Structure update

---
+++ src/main/java/lk/ac/iit/Exceptions/CustomerNotFoundException.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lk.ac.iit.Exceptions;
+
+/**
+ *
+ * @author melzjoanne
+ */
+public class CustomerNotFoundException {
+    
+}
